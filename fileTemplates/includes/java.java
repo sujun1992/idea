c118b34d3ecc64d00d036400d44b7eecef21cb2c
sujun <sujun5@asiainfo-sec.com>
2019-06-17 15:14:09 +0800

    IU-2019.1 <76321@sujun Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, git.xml, ide-features-trainer.xml, print.xml, java.java, project.default.xml, diff.xml, lombok-plugin.xml, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, remote-servers.xml, androidEditors.xml, plugin_ui.xml, colors.scheme.xml, _@user_Darcula.icls, mygroup.xml, databaseDrivers.xml, templates.xml, Default.xml, intellij-javadocs.xml, scala.xml, laf.xml, docker.xml, jdk.table.xml, path.macros.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/java.java
@@ -0,0 +1,8 @@
+/**   
+* @Title: ${file_name} 
+* @Package ${package_name} 
+* @Description: ${todo}(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date ${date} ${time} 
+* @version V1.0   
+*/
